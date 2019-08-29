375551c848fbcbd883a136594fbff5694b99bc32
YichengChen <yichengchen99@gmail.com>
2019-08-29 17:49:23 +0800

    server json request parsing

--- server/JsonUtils/JsonUtils.cpp
+++ server/JsonUtils/JsonUtils.cpp
@@ -10,15 +10,15 @@
 
 
 //checked
-bool JsonUtils::parse_request_token(char *s_json, char *&uId, char *&uPwd) {
+bool JsonUtils::parse_request_token(char *s_json, int *uId, char *&uPwd) {
     cJSON * json_root;
     cJSON * json_requestToken;
 
     json_root = cJSON_Parse(s_json);
     json_requestToken = cJSON_GetObjectItem(json_root,"requestToken");
 
-    uId = cJSON_Print(cJSON_GetObjectItem(json_requestToken,"uId"));
-    uPwd = cJSON_Print(cJSON_GetObjectItem(json_requestToken,"uPWd"));
+    (*uId) = cJSON_GetObjectItem(json_requestToken,"uId")->valueint;
+    uPwd = cJSON_GetObjectItem(json_requestToken,"uPWd")->valuestring;
 
     cJSON_Delete(json_root);
     return true;
@@ -29,7 +29,7 @@ bool JsonUtils::parse_request_type(char *s_json, char *&requestType) {
     cJSON * json_root;
 
     json_root = cJSON_Parse(s_json);
-    requestType = cJSON_Print(cJSON_GetObjectItem(json_root,"requestType"));
+    requestType = cJSON_GetObjectItem(json_root,"requestType")->valuestring;
 
     cJSON_Delete(json_root);
     return true;
@@ -43,8 +43,8 @@ bool JsonUtils::parse_request_register_json(char *s_json, char *&username, char 
     json_root = cJSON_Parse(s_json);
     json_requestContent = cJSON_GetObjectItem(json_root, "requestContent");
 
-    username = cJSON_Print(cJSON_GetObjectItem(json_requestContent, "username"));
-    password = cJSON_Print(cJSON_GetObjectItem(json_requestContent, "password"));
+    username = cJSON_GetObjectItem(json_requestContent, "username")->valuestring;
+    password = cJSON_GetObjectItem(json_requestContent, "password")->valuestring;
 
     cJSON_Delete(json_root);
     return true;
@@ -57,8 +57,8 @@ bool JsonUtils::parse_request_login_json(char *s_json, char *&username, char *&p
     json_root = cJSON_Parse(s_json);
     json_requestContent = cJSON_GetObjectItem(json_root, "requestContent");
 
-    username = cJSON_Print(cJSON_GetObjectItem(json_requestContent, "username"));
-    password = cJSON_Print(cJSON_GetObjectItem(json_requestContent, "password"));
+    username = cJSON_GetObjectItem(json_requestContent, "username")->valuestring;
+    password = cJSON_GetObjectItem(json_requestContent, "password")->valuestring;
 
     cJSON_Delete(json_root);
     return true;
@@ -70,29 +70,29 @@ bool JsonUtils::parse_request_getInfo_json(char *s_json) {
 }
 
 //checked
-bool JsonUtils::parse_request_getMessages_json(char *s_json, char *&lastCalledMessage) {
+bool JsonUtils::parse_request_getMessages_json(char *s_json, int *lastCalledMessage) {
     cJSON *json_root;
     cJSON *json_requestContent;
 
     json_root = cJSON_Parse(s_json);
     json_requestContent = cJSON_GetObjectItem(json_root, "requestContent");
 
-    lastCalledMessage = cJSON_Print(cJSON_GetObjectItem(json_requestContent, "lastCalledMessage"));
+    (*lastCalledMessage) = cJSON_GetObjectItem(json_requestContent, "lastCalledMessage")->valueint;
 
     cJSON_Delete(json_root);
     return true;
 }
 
 //checked
-bool JsonUtils::parse_request_sendMessages_json(char *s_json, char *&utoId, char *&content) {
+bool JsonUtils::parse_request_sendMessages_json(char *s_json, int *utoId, char *&content) {
     cJSON *json_root;
     cJSON *json_requestContent;
 
     json_root = cJSON_Parse(s_json);
     json_requestContent = cJSON_GetObjectItem(json_root, "requestContent");
 
-    utoId = cJSON_Print(cJSON_GetObjectItem(json_requestContent, "utoId"));
-    content = cJSON_Print(cJSON_GetObjectItem(json_requestContent, "content"));
+    (*utoId) = cJSON_GetObjectItem(json_requestContent, "utoId")->valueint;
+    content = cJSON_GetObjectItem(json_requestContent, "content")->valuestring;
 
     cJSON_Delete(json_root);
     return true;
