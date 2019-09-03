0ce991cdeb4c1bef287cb7ea8f115012a6543cb3
soshuaiD <947783684@qq.com>
2019-09-02 20:28:44 -0700

    MainView changed

--- client/View/MainView/List/GroupList/GroupList.cpp
+++ client/View/MainView/List/GroupList/GroupList.cpp
@@ -1,5 +1,5 @@
 //
-// Created by William Zhang on 2019-09-02.
+// Created by dd on 8/31/19.
 //
 
 #include <string>
@@ -15,32 +15,37 @@ GroupList::GroupList() {
                                                 "id",renderer,
                                                 "text",GROUP_ID,
                                                 NULL);
+    gtk_tree_view_insert_column_with_attributes(GTK_TREE_VIEW(view),
+                                                GROUP_MESSAGE,
+                                                "message",renderer,
+                                                "text",GROUP_MESSAGE,
+                                                NULL);
 }
 
 GtkWidget *GroupList::getView() {
     return view;
 }
 
-void GroupList::append(const gchar *id) {
+GtkTreeModel *GroupList::list_model_create() {
+    GtkListStore *store;
+    store = gtk_list_store_new(GROUP_COLUMNS,G_TYPE_STRING, G_TYPE_STRING);
+    return GTK_TREE_MODEL(store);
+}
+
+void GroupList::append(const gchar *id, const gchar *message) {
     GtkListStore *store;
     GtkTreeIter iter;
 
     store = GTK_LIST_STORE(gtk_tree_view_get_model(GTK_TREE_VIEW(view)));
     gtk_list_store_append(store, &iter);
-    gtk_list_store_set(store, &iter,GROUP_ID,id, -1);
+    gtk_list_store_set(store, &iter,GROUP_ID,id,GROUP_MESSAGE,message, -1);
 }
 
-void GroupList::setData(const std::vector<Group>& groups) {
+void GroupList::setData(const std::vector<Group>& contacts) {
     GtkListStore *store;
     store = GTK_LIST_STORE(gtk_tree_view_get_model(GTK_TREE_VIEW(view)));
     gtk_list_store_clear(store);
-    for (auto i : groups) {
-        append(std::to_string(i.getGId()).c_str());
+    for (auto i : contacts) {
+        append(std::to_string(i.getGId()).c_str(), "");
     }
-}
-
-GtkTreeModel *GroupList::list_model_create() {
-    GtkListStore *store;
-    store = gtk_list_store_new(GROUP_COLUMNS,G_TYPE_STRING);
-    return GTK_TREE_MODEL(store);
 }
--- client/View/MainView/MainView.cpp
+++ client/View/MainView/MainView.cpp
@@ -3,18 +3,19 @@
 //
 
 #include "MainView.h"
+#include "List/FriendList/FriendList.h"
 #include <gtk/gtk.h>
 #include <string>
 
 
-MainView::MainView():mlist(),flist(), gList() {
+MainView::MainView():flist(),glist(){
 }
 
 
 void MainView::show()
 {
     gtk_widget_show_all(main_window);
-    ChangeToMessage(nullptr, this);
+    ChangeToFriend(nullptr, this);
 }
 
 void MainView::destroy() {
@@ -22,35 +23,23 @@ void MainView::destroy() {
 
 }
 
-void MainView::ChangeToMessage(GtkWidget *widget, gpointer data) {
-    MainView * mainView = (MainView *) data;
-    gtk_widget_show(mainView->mlist.getView());
-    gtk_widget_hide(mainView->flist.getView());
-    gtk_widget_hide(mainView->gList.getView());
-    gtk_widget_hide(mainView->inputHBox);
-    mainView->mode = MSG;
-}
-
-
 void MainView::ChangeToFriend(GtkWidget *widget, gpointer data) {
     MainView * mainView = (MainView *) data;
-    gtk_widget_show(mainView->inputHBox);
     gtk_entry_set_text(GTK_ENTRY(mainView->inputEntry), "");
     gtk_widget_show(mainView->flist.getView());
-    gtk_widget_hide(mainView->mlist.getView());
-    gtk_widget_hide(mainView->gList.getView());
+    gtk_widget_hide(mainView->glist.getView());
     mainView->mode = CONTACTS;
 }
 
+
 void MainView::ChangeToGroup(GtkWidget *widget, gpointer data) {
     MainView * mainView = (MainView *) data;
-    gtk_widget_show(mainView->inputHBox);
     gtk_entry_set_text(GTK_ENTRY(mainView->inputEntry), "");
+    gtk_widget_show(mainView->glist.getView());
     gtk_widget_hide(mainView->flist.getView());
-    gtk_widget_hide(mainView->mlist.getView());
-    gtk_widget_show(mainView->gList.getView());
     mainView->mode = GROUP;
 }
+
 
 
 void MainView::create() {
@@ -62,11 +51,11 @@ void MainView::create() {
     GtkWidget *list_box;
     GtkWidget *image_avatar;
     GtkWidget *label_name;
-    GtkWidget *message_button;
+
     GtkWidget *friend_button;
     GtkWidget * group_button;
+
     GtkWidget * inputConfirmButton;
-    GtkTreeSelection *message_selection;
     GtkTreeSelection *friend_selection;
     GtkTreeSelection *group_selection;
 
@@ -81,12 +70,10 @@ void MainView::create() {
     label_name=gtk_label_new("dd");
     button_box = gtk_hbox_new(FALSE,0);
     list_box = gtk_vbox_new(FALSE,0);
-    message_button = gtk_button_new_with_label("Messages");
     friend_button=gtk_button_new_with_label("Contacts");
     group_button = gtk_button_new_with_label("Groups");
-    message_selection = gtk_tree_view_get_selection(GTK_TREE_VIEW(mlist.getView()));
     friend_selection = gtk_tree_view_get_selection(GTK_TREE_VIEW(flist.getView()));
-    group_selection = gtk_tree_view_get_selection(GTK_TREE_VIEW(gList.getView()));
+    group_selection = gtk_tree_view_get_selection(GTK_TREE_VIEW(glist.getView()));
 
     inputHBox = gtk_hbox_new(FALSE, 0);
     inputEntry = gtk_entry_new();
@@ -96,16 +83,13 @@ void MainView::create() {
      */
     g_signal_connect(G_OBJECT(main_window), "delete_event",
                      G_CALLBACK(gtk_main_quit), NULL);
-    g_signal_connect(G_OBJECT(message_button),"clicked",
-                     G_CALLBACK(ChangeToMessage), this);
     g_signal_connect(G_OBJECT(friend_button),"clicked",
                      G_CALLBACK(ChangeToFriend), this);
     g_signal_connect(G_OBJECT(group_button),"clicked",
                      G_CALLBACK(ChangeToGroup), this);
     g_signal_connect(G_OBJECT(inputConfirmButton),"clicked",
                      G_CALLBACK(goButtonClickedCallback),this);
-    g_signal_connect(G_OBJECT(message_selection),"changed",
-                    G_CALLBACK(tree_selection_message_changed),this);
+
     g_signal_connect(G_OBJECT(friend_selection),"changed",
                     G_CALLBACK(tree_selection_friend_changed),this);
     g_signal_connect(G_OBJECT(group_selection),"changed",
@@ -118,16 +102,12 @@ void MainView::create() {
     gtk_window_set_default_size(GTK_WINDOW(main_window),300,600);
     gtk_window_set_position(GTK_WINDOW(main_window),GTK_WIN_POS_CENTER);
     gtk_widget_set_size_request(info_box,300,100);
-    gtk_widget_set_size_request(mlist.getView(),300,500);
-    gtk_button_set_relief(GTK_BUTTON(message_button),GTK_RELIEF_NONE);
     gtk_button_set_relief(GTK_BUTTON(friend_button),GTK_RELIEF_NONE);
     gtk_button_set_relief(GTK_BUTTON(group_button),GTK_RELIEF_NONE);
 
     /**
      * 添加构件
      */
-
-    //gtk_container_add(GTK_CONTAINER(main_window),scolled);
 
     gtk_box_pack_start(GTK_BOX(info_box),image_avatar,FALSE,FALSE,50);
     gtk_box_pack_start(GTK_BOX(info_box),label_name,FALSE,FALSE,5);
@@ -136,9 +116,8 @@ void MainView::create() {
     gtk_box_pack_start(GTK_BOX(inputHBox), inputEntry, TRUE, TRUE, 0);
     gtk_box_pack_start(GTK_BOX(inputHBox), inputConfirmButton, FALSE, FALSE, 0);
 
-    gtk_box_pack_start(GTK_BOX(button_box),message_button,FALSE,FALSE,50);
-    gtk_box_pack_start(GTK_BOX(button_box),friend_button,FALSE,FALSE,5);
-    gtk_box_pack_start(GTK_BOX(button_box), group_button, FALSE, FALSE, 5);
+    gtk_box_pack_start(GTK_BOX(button_box),friend_button,TRUE,TRUE,5);
+    gtk_box_pack_start(GTK_BOX(button_box), group_button, TRUE, TRUE, 5);
 
     gtk_box_pack_start(GTK_BOX(vbox),button_box,FALSE,FALSE,0);
     gtk_box_pack_start(GTK_BOX(vbox),scolled,TRUE,TRUE,0);
@@ -146,28 +125,15 @@ void MainView::create() {
 
 
     gtk_box_pack_start(GTK_BOX(list_box), inputHBox, FALSE, FALSE, 0);
-    gtk_box_pack_start(GTK_BOX(list_box),mlist.getView(),TRUE,TRUE,5);
     gtk_box_pack_start(GTK_BOX(list_box),flist.getView(),TRUE,TRUE,5);
-    gtk_box_pack_start(GTK_BOX(list_box),gList.getView(),TRUE,TRUE,5);
+    gtk_box_pack_start(GTK_BOX(list_box),glist.getView(),TRUE,TRUE,5);
 
+    flist.append("1","boss","nb!!!!");
+    flist.append("2","dd","handsome");
+
+    glist.append("1","who cares ");
+    glist.append("2","jesus fucking christ");
     gtk_container_add(GTK_CONTAINER(main_window),vbox);
-}
-
-void MainView::tree_selection_message_changed(GtkTreeSelection *selection, gpointer data) {
-    GtkTreeIter iter;
-    GtkTreeModel *model;
-
-    gchar *name;
-    gchar *message;
-
-    if(gtk_tree_selection_get_selected(selection,&model,&iter))
-    {
-        gtk_tree_model_get(model,&iter,COLUMN_NAME,&name,COLUMN_MESSAGE,&message,-1);
-        g_print("name = %s\n",name);
-        g_print("message = %s\n",message);
-        g_free(name);
-        g_free(message);
-    }
 }
 
 void MainView::tree_selection_friend_changed(GtkTreeSelection *selection, gpointer data) {
@@ -179,13 +145,15 @@ void MainView::tree_selection_friend_changed(GtkTreeSelection *selection, gpoint
 
     if(gtk_tree_selection_get_selected(selection,&model,&iter))
     {
-        gtk_tree_model_get(model,&iter,FRIEND_ID,&id,FRIEND_NAME,&name,-1);
+
+        gtk_tree_model_get(model,&iter,USER_ID,&id,USER_NAME,&name,-1);
         g_print("id = %s\n",id);
         g_print("name = %s\n",name);
         int selectedId = std::stoi(id);
-        ((MainView *)data)->callback->selectUser(selectedId, name);
-        g_free(name);
+        std::string Name = name;
+        ((MainView *)data)->callback->selectUser(selectedId,Name);
         g_free(id);
+        g_free(name);
     }
 }
 
@@ -194,14 +162,17 @@ void MainView::tree_selection_group_changed(GtkTreeSelection *selection, gpointe
     GtkTreeModel *model;
 
     gchar *id;
+    gchar *message;
 
     if(gtk_tree_selection_get_selected(selection,&model,&iter))
     {
-        gtk_tree_model_get(model,&iter,GROUP_ID,&id,-1);
+        gtk_tree_model_get(model,&iter,GROUP_ID,&id,GROUP_MESSAGE,&message,-1);
         g_print("id = %s\n",id);
+        g_print("name = %s\n",message);
         int selectedId = std::stoi(id);
         ((MainView *)data)->callback->selectGroup(selectedId);
         g_free(id);
+        g_free(message);
     }
 }
 
