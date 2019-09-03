eb7f0b06e5335bf8da7d3722a429876e2072407c
YichengChen <yichengchen99@gmail.com>
2019-09-03 09:48:34 +0800

    改成只用在开始前打开链接了

--- server/NetworkUtils/NetworkUtils.cpp
+++ server/NetworkUtils/NetworkUtils.cpp
@@ -216,14 +216,14 @@ void NetworkUtils::start_server() {
                         char *username;
                         char *password;
 
-                        JsonUtils::parse_request_register_json(buffer,username,password);
+//                        JsonUtils::parse_request_register_json(buffer,username,password);
 
 //                        int uId = DatabaseUtils::doRegister(username,password);
-                        int uId = 1;
+//                        int uId = 1;
 
-                        char *s_json = JsonUtils::make_response_register_json(TRUE_CONTENT,uId);
+//                        char *s_json = JsonUtils::make_response_register_json(TRUE_CONTENT,uId);
 
-                        send(fd,s_json,strlen(s_json),0);
+//                        send(fd,s_json,strlen(s_json),0);
                     }else if(strcmp(requestType,TYPE_LOGIN)==0){
                         /**
                          * online
